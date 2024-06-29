6eed7a052c6ca4ef4f8f4cfa4aa9992718072419
Viktor Korshunov <viktorkorshunov@MacBook-Air-Viktor.local>
2024-06-29 12:53:30 +0200

    render fetched info

--- script.js
+++ script.js
@@ -1,19 +1,28 @@
-function addHTMLElements() {
-  const div = document.querySelector(".container");
-  const form = document.createElement("form");
-  div.appendChild(form);
-  const input = document.createElement("input");
-  form.appendChild(input);
-  const button = document.createElement("button");
-  form.appendChild(button);
-  button.textContent = "Search";
-}
+const div = document.querySelector(".container");
+const form = document.createElement("form");
+div.appendChild(form);
+const input = document.createElement("input");
+form.appendChild(input);
+const button = document.createElement("button");
+form.appendChild(button);
+button.textContent = "Search";
+button.setAttribute("type", "submit");
+const p = document.createElement("p");
+div.appendChild(p);
 
-function getLocation() {
-  const city = prompt("Write your city");
-  console.log(city);
-  return city;
-}
+button.addEventListener("click", (e) => {
+  e.preventDefault();
+  const city = input.value;
+  getForecast(city);
+});
+
+const weatherData = {
+  country: "",
+  locationName: "",
+  celciumDegree: "",
+};
+
+let { country, locationName, celciumDegree } = weatherData;
 
 async function getForecast(city) {
   try {
@@ -21,16 +30,16 @@ async function getForecast(city) {
       `http://api.weatherapi.com/v1/current.json?key=330e43a6758846a7a2783931242906&q=${city}`,
       { mode: "cors" }
     );
-    if (!response.hasOwnProperty(name)) {
-      throw new Error("Please enter correct location");
-    }
+
     const data = await response.json().then((res) => {
       console.log(res.location.country);
+      country = res.location.country;
+      locationName = res.location.name;
+      celciumDegree = res.current.temp_c;
+
+      p.textContent = `City: ${locationName}, Country: ${country}, Weather in C: ${celciumDegree}`;
     });
   } catch (e) {
     console.log(e);
   }
 }
-addHTMLElements();
-const city = getLocation();
-getForecast(city);
